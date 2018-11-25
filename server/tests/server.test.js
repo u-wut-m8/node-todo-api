fd4c45f20d7fb3512b95888433af6ca64a72d797
Sumanmit Padhi <sumanmit.padhi@outlook.com>
2018-11-25 20:20:45 +0530

    Add test for PATCH /todos/:id.

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -7,7 +7,9 @@ var {ObjectID} = require("mongodb");
 
 const todos = [{
   _id: new ObjectID(),
-  text: "Test data 1."
+  text: "Test data 1.",
+  completed: true,
+  completedAt: 345
 }, {
   _id: new ObjectID(),
   text: "Test data 2."
@@ -125,5 +127,39 @@ describe("DELETE /todos/:id", () => {
     .delete(`/todos/123abc`)
     .expect(404)
     .end(done);
+  });
+});
+
+describe("PATCH /todos/:id", () => {
+  it("should clear completedAt when todo is not completed", (done) => {
+    var id = todos[0]._id.toHexString();
+    var text = "New text.";
+    supertest(app)
+    .patch(`/todos/${id}`)
+    .send({
+      completed: false,
+      text
+    }).expect(200)
+    .expect((res) => {
+      expect(res.body.todo.text).toBe(text);
+      expect(res.body.todo.completed).toBe(false);
+      expect(res.body.todo.completedAt).toNotExist();
+    }).end(done);
+  });
+
+  it("should update the todo", (done) => {
+    var id = todos[1]._id.toHexString();
+    var text = "New text.";
+    supertest(app)
+    .patch(`/todos/${id}`)
+    .send({
+      completed: true,
+      text
+    }).expect(200)
+    .expect((res) => {
+      expect(res.body.todo.text).toBe(text);
+      expect(res.body.todo.completed).toBe(true);
+      expect(res.body.todo.completedAt).toBeA("number");
+    }).end(done);
   });
 });
